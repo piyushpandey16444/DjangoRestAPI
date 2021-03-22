fcae4304cd2b2a8b861045e38b10166801892dff
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-22 18:18:17 +0530

    new app updated.

--- authApp/urls.py
+++ authApp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import index
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', include("djoser.urls")),
+    path('', include("djoser.urls.authtoken"))
 ]
--- restApi/settings.py
+++ restApi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'authApp',
     'rest_framework',
+    'rest_framework.authtoken',
     'djoser',
 ]
 
