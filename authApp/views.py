9d69a731d81cc0676198f6fe37abe8f7d4e12763
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-22 18:04:41 +0530

    views created for returning date

--- authApp/views.py
+++ authApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import datetime
 
-# Create your views here.
+
+def index(request):
+    date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    message = 'server is live current time is: '
+    return Response(data=message + date, status=status.HTTP_200_OK)
